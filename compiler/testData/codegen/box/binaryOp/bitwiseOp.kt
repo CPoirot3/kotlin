440af17d7ca5bbcc0ac4d4c8add04a41222f6d0f
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-09-28 07:26:55 +0300

    ~ use var to prevent constant evaluation

--- compiler/testData/codegen/box/binaryOp/bitwiseOp.kt
+++ compiler/testData/codegen/box/binaryOp/bitwiseOp.kt
@@ -1,15 +1,15 @@
 fun box(): String {
     // D = 1101 C = 1100
     // 6 = 0110 5 = 0101
-    val iarg1: Int = 0xDC56DC56.toInt()
-    val iarg2: Int = 0x65DC65DC
-    val i1 = iarg1 and iarg2
-    val i2 = iarg1 or  iarg2
-    val i3 = iarg1 xor iarg2
-    val i4 = iarg1.inv()
-    val i5 = iarg1 shl 16
-    val i6 = iarg1 shr 16
-    val i7 = iarg1 ushr 16
+    var iarg1: Int = 0xDC56DC56.toInt()
+    var iarg2: Int = 0x65DC65DC
+    var i1 = iarg1 and iarg2
+    var i2 = iarg1 or  iarg2
+    var i3 = iarg1 xor iarg2
+    var i4 = iarg1.inv()
+    var i5 = iarg1 shl 16
+    var i6 = iarg1 shr 16
+    var i7 = iarg1 ushr 16
 
     if (i1 != 0x44544454.toInt()) return "fail: Int.and"
     if (i2 != 0xFDDEFDDE.toInt()) return "fail: Int.or"
@@ -21,15 +21,15 @@ fun box(): String {
 
 
     // TODO: Use long hex constants after KT-4749 is fixed
-    val larg1: Long = (0xDC56DC56L shl 32) + 0xDC56DC56 // !!!!
-    val larg2: Long = 0x65DC65DC65DC65DC
-    val l1 = larg1 and larg2
-    val l2 = larg1 or  larg2
-    val l3 = larg1 xor larg2
-    val l4 = larg1.inv()
-    val l5 = larg1 shl 32
-    val l6 = larg1 shr 32
-    val l7 = larg1 ushr 32
+    var larg1: Long = (0xDC56DC56L shl 32) + 0xDC56DC56 // !!!!
+    var larg2: Long = 0x65DC65DC65DC65DC
+    var l1 = larg1 and larg2
+    var l2 = larg1 or  larg2
+    var l3 = larg1 xor larg2
+    var l4 = larg1.inv()
+    var l5 = larg1 shl 32
+    var l6 = larg1 shr 32
+    var l7 = larg1 ushr 32
     
     if (l1 != 0x4454445444544454) return "fail: Long.and"
     if (l2 != (0xFDDEFDDEL shl 32) + 0xFDDEFDDE) return "fail: Long.or"
@@ -39,15 +39,15 @@ fun box(): String {
     if (l6 != (0xFFFFFFFFL shl 32) + 0xDC56DC56) return "fail: Long.shr"
     if (l7 != (0x00000000L shl 32) + 0xDC56DC56.toLong()) return "fail: Long.ushr"
 
-    val sarg1: Short = 0xDC56.toShort()
-    val sarg2: Short = 0x65DC.toShort()
-    val s1 = sarg1 and sarg2
-    val s2 = sarg1 or  sarg2
-    val s3 = sarg1 xor sarg2
-    val s4 = sarg1.inv()
-    val s5 = sarg1 shl 8
-    val s6 = sarg1 shr 8
-    val s7 = sarg1 ushr 8
+    var sarg1: Short = 0xDC56.toShort()
+    var sarg2: Short = 0x65DC.toShort()
+    var s1 = sarg1 and sarg2
+    var s2 = sarg1 or  sarg2
+    var s3 = sarg1 xor sarg2
+    var s4 = sarg1.inv()
+    var s5 = sarg1 shl 8
+    var s6 = sarg1 shr 8
+    var s7 = sarg1 ushr 8
     
     if (s1 != 0x4454.toShort()) return "fail: Short.and"
     if (s2 != 0xFDDE.toShort()) return "fail: Short.or"
@@ -57,15 +57,15 @@ fun box(): String {
     if (s6 != 0xFFDC.toShort()) return "fail: Short.shr"
     // TODO: if (s7 != 0x00DC.toShort()) return "fail: Short.ushr"
 
-    val barg1: Byte = 0xDC.toByte()
-    val barg2: Byte = 0x65.toByte()
-    val b1 = barg1 and barg2
-    val b2 = barg1 or  barg2
-    val b3 = barg1 xor barg2
-    val b4 = barg1.inv()
-    val b5 = barg1 shl 4
-    val b6 = barg1 shr 4
-    val b7 = barg1 ushr 4
+    var barg1: Byte = 0xDC.toByte()
+    var barg2: Byte = 0x65.toByte()
+    var b1 = barg1 and barg2
+    var b2 = barg1 or  barg2
+    var b3 = barg1 xor barg2
+    var b4 = barg1.inv()
+    var b5 = barg1 shl 4
+    var b6 = barg1 shr 4
+    var b7 = barg1 ushr 4
     
     if (b1 != 0x44.toByte()) return "fail: Byte.and"
     if (b2 != 0xFD.toByte()) return "fail: Byte.or"
